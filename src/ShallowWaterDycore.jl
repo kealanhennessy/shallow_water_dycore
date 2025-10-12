2b80909972e74c919f56da1bbd7ca53e286ec9c4
Kealan John Hennessy <kjh2182@columbia.edu>
2025-10-12 17:11:27 -0400

    Added simple spherical grid construction. Laid out basic call stack in run_model.

--- src/ShallowWaterDycore.jl
+++ src/ShallowWaterDycore.jl
@@ -1,10 +1,30 @@
 module shallowwaterdycore
 
 include("grid.jl")
-include("field.jl")
+include("fields.jl")
 include("dynamics.jl")
 include("time_integration.jl")
 include("boundary_conditions.jl")
 include("diffusion.jl")
 include("diagnostics.jl")
 include("io.jl")
+
+export run_model
+
+function run_model(params)
+    grid = build_grid(params)
+
+    state = init_state(params)
+
+    t = 0.0
+    while t < params.tmax
+        tendencies = compute_tendencies(grid, state, params)
+
+        state = advance_state(grid, state, params, tendencies)
+
+        apply_boundary_conditions!(grid, state params)
+
+        t += params.dt
+    end # main time stepping loop
+
+end # run_model
---
+++ src/grid.jl
@@ -0,0 +1,36 @@
+module grid
+
+export build_grid
+
+struct grid
+    nx::Int           # number of longitude points
+    ny::Int           # number of latitude points
+    lon::Array{Float64,1}  # longitude array [rad]
+    lat::Array{Float64,1}  # latitude array [rad]
+    dlon::Float64     # grid spacing in longitude
+    dlat::Float64     # grid spacing in latitude
+    coslat::Array{Float64,1} # cos(lat) for metric factors
+    cell_area::Array{Float64,2} # area of each cell
+end 
+
+function build_grid(params)
+    nx, ny = params.nx, params.ny
+
+    lon = range(0, 2*pi, length=ny)[begin:end-1] # chop of the end (periodic)
+    lat = range(-pi/2 + pi/(2*ny), pi/2 - pi/(2*ny), length=ny) # chop off the ends (poles)
+
+    dlon = lon[2] - lon[1]
+    dlat = lat[2] - lat[1]
+
+    coslat = cos.(lat)
+
+    a = params.a
+    cell_area = zeros(nx, ny)
+    for i = 1:nx
+        for j = 1:ny
+            cell_area[i,j] = (a^2) * dlon * dlat * coslat[j]
+        end
+    end
+
+    return grid(nx, ny, lon, lat, dlon, dlat, coslat, cell_area)
+end
